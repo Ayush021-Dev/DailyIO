ec2027d65dc725f4be38364d7a950522639b01e2
Ayush <shrivastavayush511@gmail.com>
2025-04-01 01:02:17 +0530

    finishing

--- Dailyio/src/home.js
+++ Dailyio/src/home.js
@@ -7,12 +7,10 @@ const DailyIO = () => {
     <div className="home-page">
       <div className="home-wrapper">
         <div className="home-container">
-          {/* Landing Section */}
           <div className="home-landing-container">
             <div className="home-logo"></div>
             <div className="home-title">Your Daily Dose of Fun, Facts & Fresh Updates!</div>
 
-            {/* Authentication Buttons */}
             <div className="home-auth-container">
               <div className="home-button-row">
                 <Link to="/login" className="home-guest-btn">Log In</Link>
