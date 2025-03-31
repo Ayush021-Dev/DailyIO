e4e25cc6b2b6adf545f5e10ac72dd7aee172af7a
Ayush <shrivastavayush511@gmail.com>
2025-04-01 00:44:22 +0530

    card removed

--- Dailyio/src/home.js
+++ Dailyio/src/home.js
@@ -7,16 +7,6 @@ const DailyIO = () => {
     <div className="home-page">
       <div className="home-wrapper">
         <div className="home-container">
-          {/* Quote Section */}
-          <div className="home-quote-section">
-            <div className="home-card">
-              <h2>Daily Inspiration</h2>
-              <div className="home-quote">
-                "The only way to do great work is to love what you do." - Steve Jobs
-              </div>
-            </div>
-          </div>
-
           {/* Landing Section */}
           <div className="home-landing-container">
             <div className="home-logo"></div>
@@ -29,16 +19,6 @@ const DailyIO = () => {
                 <Link to="/signup" className="home-guest-btn">Sign In</Link>
               </div>
               <Link to="/dashboard" className="home-guest-btn">Log In as Guest</Link>
-            </div>
-          </div>
-
-          {/* Fun Facts Section */}
-          <div className="home-fun-facts">
-            <div className="home-card">
-              <h2>Fun Facts</h2>
-              <div className="home-fun-fact">
-                Honey never spoils. Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old!
-              </div>
             </div>
           </div>
         </div>
