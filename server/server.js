42c19be759057871e5aa67dabfc19d42ff559fb0
Ayush <shrivastavayush511@gmail.com>
2025-04-14 04:10:56 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -6,8 +6,14 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const app = express();
 
-// Simple CORS
-app.use(cors());
+// Enable CORS for all origins
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 
 // Add a basic route for health checks
