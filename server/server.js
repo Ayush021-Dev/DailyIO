4ba8e7283d6a49f1b8f57f19185387f720519d6a
Ayush <shrivastavayush511@gmail.com>
2025-04-15 01:01:03 +0530

    fixing

--- server/server.js
+++ server/server.js
@@ -7,12 +7,12 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 // Enable CORS for all origins
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// Use the cors package properly
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://your-frontend-domain.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(express.json());
 
